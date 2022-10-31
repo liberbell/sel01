c4f0f9005d7273820a22303011259f53f582d76e
Liberbell <liberbell@gmail.com>
2022-10-31 09:09:20 +0900

    instcheck6

--- instcheck.py
+++ instcheck.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from PIL import Image
 
-# browser = webdriver.Safari()
+browser = webdriver.Safari()
 # browser = webdriver.Chrome()
+
+browser.get("https://scraping-for-begginer.herokuapp.com/login_page")
