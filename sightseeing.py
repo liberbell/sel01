c904971ea9f4eded95c5a37318723163a9a55c9b
Liberbell <liberbell@gmail.com>
2022-11-04 08:21:17 +0900

    sightseeing1

--- sightseeing.py
+++ sightseeing.py
@@ -55,10 +55,10 @@ elem_ranking1 = elem_rankingbox1.find_element(By.CLASS_NAME, value="u_rankBox")
 # print(type(elem_ranking1.text))
 
 elem_fun1 = elem_rankingbox1.find_element(By.CLASS_NAME, value="u_categoryTipsItem").find_element(By.CLASS_NAME, value="evaluateNumber")
-print(elem_fun1.text)
 
 rank1.append(title1.split("\n")[1])
 rank1.append(elem_ranking1.text)
+rank1.append(elem_fun1.text)
 
 print(rank1)
 # browser.quit()
