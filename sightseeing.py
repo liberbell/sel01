c927c54bd654e52fe3cdbf1a389fd9e278eeb021
Liberbell <liberbell@gmail.com>
2022-11-05 22:12:07 +0900

    sightseeing11

--- sightseeing.py
+++ sightseeing.py
@@ -109,6 +109,7 @@ for elem_rankbox in elems_ranks:
 
 elem_categoryitems = []
 elems_tipsitems = browser.find_elements(By.CLASS_NAME, value="u_categoryTipsItem")
+print(len(elems_tipsitems))
 for elem_tipsitem in elems_tipsitems:
     elem_categoryitems = elem_tipsitem.find_elements(By.CLASS_NAME, value="u_title")
     print(len(elem_categoryitems))
