a21dac8deedc9b66865f82fd926883c5a8b7eed6
Liberbell <liberbell@gmail.com>
2022-11-05 21:24:14 +0900

    sightseeing9

--- sightseeing.py
+++ sightseeing.py
@@ -103,7 +103,7 @@ elems_ranks = browser.find_elements(By.CLASS_NAME, value="u_rankBox")
 for elem_rankbox in elems_ranks:
     elem_rank = elem_rankbox.find_element(By.CLASS_NAME, value="evaluateNumber")
     # print(elem_rank.text)
-    rank = elem_rank.text
+    rank = float(elem_rank.text)
     ranks.append(rank)
 # print(elems_rank)
 
