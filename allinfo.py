48fc83e9875d55483ae36b0548c524cd8e169f3f
Liberbell <liberbell@gmail.com>
2022-11-07 08:42:30 +0900

    allinfo4

--- allinfo.py
+++ allinfo.py
@@ -11,9 +11,15 @@ categories = []
 for page in range(1, 4):
     
     browser.get("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(page))
-    
+
     elem_sites = browser.find_elements(By.CLASS_NAME, value="u_areaListRankingBox")
     for elem_site in elem_sites:
         elem_title = elem_site.find_element(By.CLASS_NAME, value="u_title")
         title = elem_title.text.split("\n")[1]
         titles.append(title)
+
+    elems_ranks = browser.find_elements(By.CLASS_NAME, value="u_rankBox")
+    for elem_rankbox in elems_ranks:
+        elem_rank = elem_rankbox.find_element(By.CLASS_NAME, value="evaluateNumber")
+        rank = float(elem_rank.text)
+        ranks.append(rank)
