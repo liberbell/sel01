e2c15b977a5f1c4b1babeb403eaa19d25ef21538
Liberbell <liberbell@gmail.com>
2022-11-07 08:41:31 +0900

    allinfo3

--- allinfo.py
+++ allinfo.py
@@ -4,10 +4,16 @@ import pandas as pd
 
 browser = webdriver.Chrome()
 
-
 titles = []
 ranks = []
 categories = []
 
 for page in range(1, 4):
+    
     browser.get("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(page))
+    
+    elem_sites = browser.find_elements(By.CLASS_NAME, value="u_areaListRankingBox")
+    for elem_site in elem_sites:
+        elem_title = elem_site.find_element(By.CLASS_NAME, value="u_title")
+        title = elem_title.text.split("\n")[1]
+        titles.append(title)
