e77ba0be03fd1ba0193956555b50881d0346a11e
Liberbell <liberbell@gmail.com>
2022-11-08 08:31:34 +0900

    images6

--- images.py
+++ images.py
@@ -1,9 +1,13 @@
 from PIL import Image
+from selenium import webdriver
 
-img = Image.open("aircraft.jpg")
+# img = Image.open("aircraft.jpg")
 # img.show()
 
-img = img.resize((1024, 768))
-img.show()
-print(img.size)
-img.save("sample_resize.jpg")
+# img = img.resize((1024, 768))
+# img.show()
+# print(img.size)
+# img.save("sample_resize.jpg")
+
+browser = webdriver.Chrome()
+browser.get("https://scraping-for-beginner.herokuapp.com/image")
