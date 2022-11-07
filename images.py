08dadfb9013968bc12b8752e279f100c9c21fefd
Liberbell <liberbell@gmail.com>
2022-11-08 08:40:49 +0900

    images10

--- images.py
+++ images.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import io
+from urllib import request
 
 # img = Image.open("aircraft.jpg")
 # img.show()
