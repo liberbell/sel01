6c529aa4bc2f7a23090d07e283118d5fcdb56978
Liberbell <liberbell@gmail.com>
2022-11-09 08:47:21 +0900

    webxpath3

--- webxpath.py
+++ webxpath.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
 URL = "https://scraping-for-beginner.herokuapp.com/"
 browser.get(URL)
 
 # /html/body/div[2]/div/div/div[1]/ul/li[1]
+xpath = "/html/body/div[2]/div/div/div[1]/ul/li[1]"
+browser.find_element()
