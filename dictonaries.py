9f36db8c9f6edaae389941071021d4e0dc48a8a5
Liberbell <liberbell@gmail.com>
2022-11-10 08:43:32 +0900

    dictonaries4

--- dictonaries.py
+++ dictonaries.py
@@ -3,3 +3,4 @@ empty_dict = []
 print(empty_dict)
 bike_owners = {"James": "Ducatti 1200", "Jacob": "Ducatti 1100", "William": "BMW 1000 RR", "Aiden": "Harley"}
 print(bike_owners)
+print(bike_owners["Aiden"])
