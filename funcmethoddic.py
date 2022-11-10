37290ef5b9801d50987b16ed940d8b8820d50976
Liberbell <liberbell@gmail.com>
2022-11-11 08:38:20 +0900

    funcmethoddic1

---
+++ funcmethoddic.py
@@ -0,0 +1,6 @@
+bike_details = {"bike_owner": "James Smith",
+                "bike_model": "Ducatti 1100",
+                "bike_price": 28140,
+                "engine_displacement": 1197}
+
+print(len(bike_details))
