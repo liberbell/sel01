a9411c0f513ab6fafaa32cd19e036b9fa7b0a728
Liberbell <liberbell@gmail.com>
2022-11-12 15:21:59 +0900

    pythonsets4

--- pythonsets.py
+++ pythonsets.py
@@ -5,5 +5,8 @@ empty_sets = set()
 print(empty_sets)
 print(type(empty_sets))
 
-mixed_set = ("Emma", 5,  1.5, True)
+mixed_set = {"Emma", 5,  1.5, True}
 print(mixed_set)
+
+mixed_set1 = {"Emma", 5,  1.5, True, (1, 2, 3)}
+print(mixed_set1)
