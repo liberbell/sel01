fb3e6b384a3c721971589fc6b35c87ac3604cb49
Liberbell <liberbell@gmail.com>
2022-11-14 08:44:47 +0900

    nestedlist5

--- nestedlist.py
+++ nestedlist.py
@@ -4,3 +4,10 @@ print(len(car_matrix))
 print(len(car_matrix[1]))
 print(car_matrix[1][0])
 print(car_matrix[2][0:2])
+
+my_list = ["Bob", 18,
+            "Eric", 15,
+            "Alex", 10,
+            "Ringo", 20
+            ]
+print(my_list)
