7aca58020274d018fb80fe9d72c8f0cc45621590
Liberbell <liberbell@gmail.com>
2022-11-13 14:44:26 +0900

    pythonsets24

--- pythonsets2.py
+++ pythonsets2.py
@@ -6,3 +6,9 @@ print(number1.union(number2))
 
 number_union = number1.union(number2, number3)
 print(number_union)
+
+student_set_1 = {"Eric", "Elton", "Bob"}
+student_set_2 = {"Bob", "George", "Alex"}
+student_set_3 = {"Alex", "Ringo", "Ed"}
+
+print(student_set_1.union(student_set_2, student_set_3))
