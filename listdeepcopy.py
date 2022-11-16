8af4d808b34a876eab8c3dd9a53c1937953200eb
Liberbell <liberbell@gmail.com>
2022-11-17 08:32:00 +0900

    listdeepcopy3

--- listdeepcopy.py
+++ listdeepcopy.py
@@ -5,4 +5,9 @@ print(names)
 
 new_names = names.copy()
 print("Original: ", names)
-print("Copied: ", new_names)
+print("Copied  : ", new_names)
+
+print(new_names[1])
+new_names[1] = "George"
+print("Original: ", names)
+print("Copied  : ", new_names)
