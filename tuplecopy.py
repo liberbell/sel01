eaa0c326933d9581c01c5c207b463ce269f6e208
Liberbell <liberbell@gmail.com>
2022-11-18 08:21:11 +0900

    tuplecopy3

--- tuplecopy.py
+++ tuplecopy.py
@@ -7,3 +7,10 @@ print("New:", new_fruits)
 
 print(new_fruits[2])
 # new_fruits[2] = "strawberry"
+
+fruits = ("apple", ["banana", "cherry"], "orange")
+# print(fruits)
+
+new_fruits = fruits
+print("Ori:", fruits)
+print("New:", new_fruits)
