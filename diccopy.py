d0bc4a0dec964aa9c500228f4a9f7ece6a7b8401
Liberbell <liberbell@gmail.com>
2022-11-18 08:39:01 +0900

    diccopy4

--- diccopy.py
+++ diccopy.py
@@ -1,3 +1,5 @@
+import copy
+
 months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5}
 print(months)
 
@@ -10,3 +12,7 @@ print(new_months["Feb"])
 new_months["Feb"] = "FEB"
 print("Ori: ", months)
 print("New: ", new_months)
+
+employees = {"Eric": 15000, "Alex": 10000, "Bob": 25, "Elton": 20000}
+
+print("Ori: ", employees)
