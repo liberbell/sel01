d8ff5d7e47424bff1e0663ad4c9b0793aad11ef3
Liberbell <liberbell@gmail.com>
2022-11-21 08:52:53 +0900

    timecomp2

--- timecomp.py
+++ timecomp.py
@@ -1,4 +1,7 @@
 def addition(num1, num2):
     total = num1 + num2
 
-    print("The sum of %d and %d is %d", num1, num2, total)
+    print("The sum of %d and %d is %d"% (num1, num2, total))
+
+
+addition(10, 15)
