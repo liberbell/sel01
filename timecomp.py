cc670167879fa3f0884fb132e99a96c1cbd516c7
Liberbell <liberbell@gmail.com>
2022-11-21 09:02:02 +0900

    timecomp4

--- timecomp.py
+++ timecomp.py
@@ -13,3 +13,7 @@ def addition(num1, num2):
     num_iterations += 1
     print("The sum of %d and %d is %d \nTotal number of iterations = %d"
             % (num1, num2, total, num_iterations))
+
+addition(20, 25)
+
+def check_oddeven(number):
