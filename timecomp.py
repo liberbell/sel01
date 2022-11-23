8dca25659cfdd28f243b6be5747267b6e6df479e
Liberbell <liberbell@gmail.com>
2022-11-23 09:20:30 +0900

    timecomp15

--- timecomp.py
+++ timecomp.py
@@ -136,7 +136,7 @@ print_pairs([11, 21, 46, 17, 68])
 def find_prime(lower, upper):
     num_iterations = 0
     for num in range(lower, upper):
-        for i in range(i, int(num / 2)):
+        for i in range(2, int(num / 2)):
             num_iterations += 1
 
             if (num % i) == 0:
