16ba18d01e1e145fe63ce10b7025bc0faf0b2e12
Liberbell <liberbell@gmail.com>
2022-11-27 17:17:25 +0900

    linkedlist1

--- linkedlist.py
+++ linkedlist.py
@@ -1,4 +1,9 @@
 class Node:
     """A node is a singly-linked list"""
 
-    def __init__(self, databal=None, nextval=None):
+    def __init__(self, dataval=None, nextval=None):
+        self.dataval = dataval
+        self.nextval = nextval
+
+    def __repr__(self):
+        return repr(self.dataval)
