a2913aa7cbcf64c21a26cf277f0baf7d319ac444
Liberbell <liberbell@gmail.com>
2022-11-30 08:39:58 +0900

    sorting7

--- sorting.py
+++ sorting.py
@@ -28,4 +28,7 @@ def bubble_sort(original_list):
     for i in range(length - 1, 0, -1):
         for index in range(i):
             if original_list[index] > original_list[index + 1]:
-                original_list[index + 1], original_list[index] = original_list[index], original_list[index + 1] 
+                original_list[index + 1], original_list[index] = original_list[index], original_list[index + 1]
+        
+        print("Unsorted till index: ", i - 1)
+        print_list(original_list)
